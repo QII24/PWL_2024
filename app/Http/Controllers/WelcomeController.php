0394b934cdfcd55faa69063f0a450858c888be3c
Rifqi Sabilillah <rifqisabililah00@gmail.com>
2024-09-06 23:02:34 +0700

    JS2_Prak3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello() 
-    {
-        return 'Hello World';
-        }
 
+
+    public function greeting(){
+        return view('hello')
+        ->with('name','Rifqi Sabilillah')
+        ->with('occupation','Doctor');
+}
 }
