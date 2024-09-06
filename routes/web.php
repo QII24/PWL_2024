00af41bece1bdd4663c3ad3e153f13b80715bb14
Rifqi Sabilillah <rifqisabililah00@gmail.com>
2024-09-06 22:30:32 +0700

    JS2_Prak1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () { 
+    return 'hello world';
+});
+
+Route::get('/world', function () { 
+    return 'world';
+});
+
+Route::get('/about', function () {
+    return 'Nama : Rifqi Sabilillah <br>NIM : 2241760082';
+ });
+
+Route::get('/user/rifqi', function($name = 'Rifqi Sabilillah') {
+    return 'Nama Saya: ' . $name;
+ });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{post}/comments/{comment}', function ($commentId) {
+    return 'Halaman Artikel ini '."dengan ID ".$commentId;
+});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
 });
